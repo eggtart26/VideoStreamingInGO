c1d0ede2f393d6ef2e42c75069224baf787dcee5
eggtart26 <justin.guan26@gmail.com>
2021-01-06 17:29:20 -0800

    implemented streamHandler

--- streamserver/handlers.go
+++ streamserver/handlers.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"io"
+	"os"
 	"io/ioutil"
 	"net/http"
 	"github.com/julienschmidt/httprouter"
 )
 
 func streamHandler(w http.ResponseWriter, r *http.ReadRequest, p httprouter.Params) {
+	vid := p.ByName("vid-id")
+	vl := VIDEO_DIR + vid
 
+	video, err := os.Open(vl)
+	if err != nil {
+		sendErrorResponse(w, http.StatusInternalServerError, "Interval Error")
+		return
+	}
+
+	w.Header().Set("Content-Type", "video/mp4")
+	http.ServeContent(w, r, "", time.Now(), video)
+
+	defer video.Close()
 }
 
 func uploadHandler(w http.ResponseWriter, r *http.ReadRequest, p httprouter.Params) {
