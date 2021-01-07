464d3862be4ec1288d3b44fba75702bef9f12c65
eggtart26 <justin.guan26@gmail.com>
2021-01-06 16:55:36 -0800

    streamserver setup

---
+++ streamserver/handlers.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"io"
+	"io/ioutil"
+	"net/http"
+	"github.com/julienschmidt/httprouter"
+)
+
+func streamHandler(w http.ResponseWriter, r *http.ReadRequest, p httprouter.Params) {
+
+}
+
+func uploadHandler(w http.ResponseWriter, r *http.ReadRequest, p httprouter.Params) {
+
+}
